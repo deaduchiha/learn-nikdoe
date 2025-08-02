178e0f0e577e0955b135b288fdcebef364374a4a
Alireza Nikzad <nikzad.alireza79@gmail.com>
2025-08-02 13:48:42 +0330

    Update translations and enhance documentation for frontend tools and accessibility. Add new sections in Persian for Git, HTML, and JavaScript fundamentals, while refining existing content for clarity and usability. Update layout to support right-to-left text direction.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
-import { Geist } from "next/font/google";
+import { Vazirmatn } from "next/font/google";
 
 import type { ReactNode } from "react";
 import type { Metadata } from "next";
@@ -9,15 +9,15 @@ export const metadata: Metadata = {
   title: "Nikode",
   description: "Your personal frontend journey — powered by Alireza Nikzad.",
 };
-const geist = Geist({
-  subsets: ["latin"],
+const vazirmatn = Vazirmatn({
+  subsets: ["arabic"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={geist.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+    <html lang="fa" className={vazirmatn.className} suppressHydrationWarning>
+      <body dir="rtl" className="flex flex-col min-h-screen">
+        <RootProvider dir="rtl">{children}</RootProvider>
       </body>
     </html>
   );
